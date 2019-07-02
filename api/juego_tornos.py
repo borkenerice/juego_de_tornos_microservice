e6a057c6d73e4f47a220392db21fc5011048bb5f
Borken <erice.borja@hotmail.com>
2019-07-02 10:22:35 +0200

    finish places functionality

--- api/juego_tornos.py
+++ api/juego_tornos.py
@@ -1,23 +1,59 @@
 import requests
 
+import config
 from flask import abort
 
 
 def find_all_places():
-    raise NotImplementedError
+    try:
+        r = requests.get(config.PLACES_ENDPOINT)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return r.status_code
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
 def find_place_by_id(place_id):
-    raise NotImplementedError
+    try:
+        r = requests.get(f'{config.PLACES_ENDPOINT}/{place_id}')
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return r.json(), r.status_code
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
-def create_place():
-    raise NotImplementedError
+def create_place(place_data):
+    try:
+        r = requests.post(f'{config.PLACES_ENDPOINT}', json=place_data)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return r.json(), r.status_code
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
 def update_place(place_id, place_data):
-    raise NotImplementedError
+    try:
+        r = requests.put(f'{config.PLACES_ENDPOINT}/{place_id}', json=place_data)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return r.json(), r.status_code
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
 def delete_place(place_id):
-    raise NotImplementedError
+    try:
+        r = requests.delete(f'{config.PLACES_ENDPOINT}/{place_id}')
+        if r.status_code == 200:
+            return f'Place {place_id} deleted', r.status_code
+        else:
+            return r.json(), r.status_code
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 
-DEBUG = True
-TESTING = True
+DEBUG = False
+TESTING = False
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SWAGGER_DIR = os.path.join(BASE_DIR, 'api', 'swagger.yml')
+
+
+# services endpoints
+PLACES_API_URL = 'http://localhost:5000/api'
+PLACES_ENDPOINT = f'{PLACES_API_URL}/place'
+
+CHARACTERS_API_URL = 'http://localhost:5000/api'
+CHARACTERS_ENDPOINT = f'{CHARACTERS_API_URL}/character'
+CHARACTERS_FIND_BY_PLACE_ENDPOINT = f'{CHARACTERS_ENDPOINT}/findByPlace'
+
+
