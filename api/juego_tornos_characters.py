1855ae6f031a7d5d78498eb921f57af46b2f0735
borken <erice.borja@hotmail.com>
2019-07-03 21:21:11 +0200

    add characters functionality

--- api/juego_tornos_characters.py
+++ api/juego_tornos_characters.py
@@ -7,13 +7,23 @@ from flask import abort
 def find_all_characters():
     try:
         r = requests.get(config.CHARACTERS_ENDPOINT)
-        return r.json()
+        if r.status_code == 200:
+            return r.json()
+        else:
+            abort(r.status_code, r.json())
     except requests.ConnectionError:
         abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
 def create_character(character_data):
-    raise NotImplementedError
+    try:
+        r = requests.post(config.CHARACTERS_ENDPOINT, json=character_data)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            abort(r.status_code, r.json())
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
 def find_characters_by_place_id(place_id):
@@ -28,12 +38,33 @@ def find_characters_by_place_id(place_id):
 
 
 def find_character_by_id(character_id):
-    raise NotImplementedError
+    try:
+        r = requests.get(f'{config.CHARACTERS_ENDPOINT}/{character_id}')
+        if r.status_code == 200:
+            return r.json()
+        else:
+            abort(r.status_code, r.json())
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
 def update_character(character_id, character_data):
-    raise NotImplementedError
+    try:
+        r = requests.put(f'{config.CHARACTERS_ENDPOINT}/{character_id}', json=character_data)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            abort(r.status_code, r.json())
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
 
 
-def delete_character_by_id(character_id, character_data):
-    raise NotImplementedError
+def delete_character_by_id(character_id):
+    try:
+        r = requests.delete(f'{config.CHARACTERS_ENDPOINT}/{character_id}')
+        if r.status_code == 200:
+            return 200
+        else:
+            abort(r.status_code, r.json())
+    except requests.ConnectionError:
+        abort(500, f'ConnectionError: A connection to the service could not be established')
